ef92002e4ebdfef3c4baf2ea6efed0081bfb1f8d
Petr77 <Petr77@github.com>
2017-03-21 14:52:49 +1100

    dob 18+ for users working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
   has_many :wines
+  validate :eighteen_plus
+
+  private
+
+  def eighteen_plus
+    if dob > (Time.now - 18.years)
+      errors.add(:dob, "must be older than 18")
+    end
+  end
 end
