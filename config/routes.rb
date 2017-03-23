dce41730a917e6b9c67c1c3dcdf32119afcd696c
Petr77 <Petr77@github.com>
2017-03-23 16:23:41 +1100

    working on styling

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   get "signin", to: "sessions#new", as: "signin"
   post "signin", to: "sessions#create"
   delete "signout", to: "sessions#destroy", as: "signout"
+
+  get "bootstrap", to: "bootstrap_demo#index"
 end
