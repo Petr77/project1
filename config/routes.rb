d2378669b4ea43f6e00312d27deeaa8adff52f92
petrsubotnik <Petr77@github.com>
2017-05-13 13:10:29 +1000

    signout

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   get "/signin", to: "sessions#new", as: "signin"
   post "/signin", to: "sessions#create"
-  delete "/signout", to: "sessions#destroy", as: "signout"
+  get "/signout", to: "sessions#destroy", as: "signout"
 
   get "/bootstrap", to: "bootstrap_demo#index"
 end
